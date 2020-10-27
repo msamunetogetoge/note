02bd75349b95282200036d19c069b4a2336c9a8f
masamune <kenjiii534@gmail.com>
2020-10-27 20:44:07 +0900

    part6

--- new_project/settings.py
+++ new_project/settings.py
@@ -76,10 +76,16 @@ WSGI_APPLICATION = 'new_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }    
 }
+
+
 
 
 # Password validation
